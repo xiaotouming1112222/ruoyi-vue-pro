aa89abbcf7b8f20e0242bc766e927a4498f5ce0f
江昕 <782113877@qq.com>
2024-08-10 20:22:35 +0800

    11
    11

--- sql/mysql/ruoyi-vue-pro.sql
+++ sql/mysql/ruoyi-vue-pro.sql
@@ -188,7 +188,7 @@ CREATE TABLE `infra_config`  (
   `visible` bit(1) NOT NULL COMMENT '是否可见',
   `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '备注',
   `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '创建者',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updater` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '' COMMENT '更新者',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
